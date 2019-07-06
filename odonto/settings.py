36ea9b4679c39c6cce84192ff6a2f3eb5d072bef
gabri <impala67>
2019-07-06 15:37:15 -0300

    Configuring the app

--- odonto/settings.py
+++ odonto/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from decouple import config
-from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
