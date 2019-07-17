ae42805ff6d6442e7ca18c04ff7877604d143cc9
RooD7 <rodrigosoualves7@gmail.com>
2019-07-17 10:57:05 -0300

    inputMask CostForm

--- financier/Forms/CostForm.py
+++ financier/Forms/CostForm.py
@@ -1,5 +1,5 @@
 from django.core.validators import RegexValidator
-from django.forms import ModelForm, Textarea, CharField, ChoiceField, Select,HiddenInput, DateInput, DateField, HiddenInput
+from django.forms import ModelForm, Textarea, CharField, DecimalField, IntegerField, ChoiceField, Select,HiddenInput, DateInput, DateField, HiddenInput
 from localflavor.br.forms import BRCPFField
 from financier.Models.Cost import Cost
 import datetime
@@ -15,10 +15,10 @@ class CostForm(ModelForm):
     cost_type = ChoiceField(choices=BOOL_COST, label="Custo",
                             initial='', widget=Select(), required=True)
     description = CharField(label='Descrição')
-    amount = CharField(label='Valor total', initial='0.00')
+    amount = CharField(label='Valor total', required=True)
     payment_form = ChoiceField(choices=BOOL_PAYMENT, label="Forma de pagamento",
                          initial='True', widget=Select(), required=True)
-    num_plots = CharField(label='Número de parcelas', widget=Textarea(attrs={'rows': 1, 'style': 'display:none;'}), initial='1', disabled=False)
+    num_plots = CharField(label='Número de parcelas', widget=Textarea(attrs={'rows': 1, 'style': 'display:none;'}), initial='1')
     payday = DateField(label='Data de vencimento')
     # payday = DateField(label='Data de vencimento', 
     #                     widget=DateInput(format='%d/%m/%Y', 
